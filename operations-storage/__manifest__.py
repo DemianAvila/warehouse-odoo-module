3d32ad6cc2331050492452fabbf8a284ff7be040
Demian Avila <demian.avila@bossa.com.mx>
2024-01-05 18:50:16 -0600

    feat: added a model and view for the shipment orders

---
+++ operations-storage/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Operations - Storage',
+    'version': '16',
+    'author': 'Demian Avila - demian.avila@bossa.com.mx',
+    'website': 'bossa.com.mx',
+    "category": "Operations",
+    "summary": 'Modlue to manage and automate the storage department task',
+    "depends": ['base', 'sale_management'],
+    'data': [
+        "views/shipment_order_views.xml"
+    ],
+    'license': 'OPL-1',
+    'active': True,
+    'installable': True,
+    'application': True,
+
+}
---
+++ operations-storage/models/shipment_order.py
@@ -0,0 +1,87 @@
+from datetime import date, datetime
+from odoo import models, fields
+
+class InverseSell2Shipment(models.Model):
+    _ihnerit = "sale.order"
+    
+    shipment_order = fields.Many2one(
+        string = "Shipment order",
+        comodel_name = "bossa.shipment.orders"
+    )
+
+class ShipmentGuides(models.Model):
+    _name = "shipment.guides"
+    _description = """Stores all of the shipment guides for printing"""
+
+    guides = fields.Binary(
+        string = "File"
+    )
+
+    order_line = fields.Many2one(
+        string = "Order line"
+        comodel_name = "sale.order.lie"
+    )
+
+class ScanerLog(models.Model):
+    _name = "scanner.log"
+    _description = """Store the tries of scan a right product when 
+    preparing the shipment order"""
+    
+    order_line = fields.Many2one(
+        string = "Order line"
+        comodel_name = "sale.order.line"
+    )
+
+    timestamp = fields.Datetime(
+        string = "Time Stamp"
+    )
+
+class ShipmentFields(models.Model):
+    _ihnerit = "sale.order.line"
+    
+    scaner_log = fields.One2many(
+        string = "Scanner log",
+        comodel_name = "scanner.log",
+        inverse_name = "order.line"
+    )
+    
+    shipment_guides = fields.One2many(
+        string = "Shipment guides"
+        comodel_name = "shipment.guides"
+        inverse_name = "order_line"
+    )
+
+    has_been_shipped = fields.Boolean(
+        string = "Shipped",
+        default = False
+    )
+
+class ShipmentOrder(models.Model):
+    _name = "bossa.shipment.orders"
+    _description = """Generates and stores the shipment orders, 
+    as well as it manages the state of the orders"""
+
+    placement_date = fields.Date(
+        string = "Placement date",
+        default = date.today()
+    )
+    
+    order_title = fields.Char(
+        string = "Order title",
+        default = f"Shipment order from {datetime.strftime(date.today(), '%d/%m/%Y')}"
+    )
+
+    sell_orders = fields.One2many(
+        string = "Sell orders",
+        comodel = "sale.order",
+        inverse_name = "shipment_order",
+    )
+
+    datetime_from = fields.Datetime(
+        string = "From"
+    )
+    
+    datetime_until =  fields.Datetime(
+        string = "Until"
+    )
+
