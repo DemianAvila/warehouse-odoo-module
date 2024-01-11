603aec3cfcc4b47a6d0925613d9034dd74acca68
Demian Avila <demian.avila@bossa.com.mx>
2024-01-10 22:52:11 -0600

    feat: test html table

--- operations_warehouse/models/shipment_order.py
+++ operations_warehouse/models/shipment_order.py
@@ -77,3 +77,34 @@ class ShipmentOrderInherit(models.Model):
         string = "Until"
     )
 
+    shipment_table = fields.Text(
+        default = """
+            <table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>John Doe</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Jane Smith</td>
+            <td>30</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Bob Johnson</td>
+            <td>22</td>
+        </tr>
+    </tbody>
+</table>
+        """
+    )
+
