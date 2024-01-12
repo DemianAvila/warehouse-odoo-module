59e80ebaf185e7b602c2160b4f598ff4163e01c4
Demian Avila <demian.avila@bossa.com.mx>
2024-01-12 00:25:12 -0600

    fix: debug test

--- operations_warehouse/models/model_extentions.py
+++ operations_warehouse/models/model_extentions.py
@@ -39,6 +39,7 @@ class InverseSell2Shipment(models.Model):
     _inherit = "sale.order"
 
     def see_product_info(self):
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
         return self.env.ref("operations_warehouse.sale_order_edit_action").read()[0]
 
     document_shipment_guides = fields.One2many(
