a5d31db64abe4f6156148989b293c9ee6cdebe1d
Demian Avila <demian.avila@bossa.com.mx>
2024-01-12 01:02:13 -0600

    fix: change model name

--- operations_warehouse/models/model_extentions.py
+++ operations_warehouse/models/model_extentions.py
@@ -9,12 +9,12 @@ class SaleOrderWizard(models.TransientModel):
     sale_order_id = fields.Many2one(
         comodel_name = "sale.order"
     )
-    documents = fields.Many2many(
-        comodel_name = "ir.attachment"
-    )
-    products = fields.Many2many(
-        comodel_name = "sale.order.lines"
-    )
+    #documents = fields.Many2many(
+    #    comodel_name = "ir.attachment"
+    #)
+    #products = fields.Many2many(
+    #    comodel_name = "sale.order.lines"
+    #)
 
 
 class Guides (models.Model):
