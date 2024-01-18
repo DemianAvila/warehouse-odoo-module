c6c89b79fa81fc4ade9aae8c387d0d50543b0d3b
Demian Avila <demian.avila@bossa.com.mx>
2024-01-17 23:58:34 -0600

    fix: corrected args in method

--- operations_warehouse/static/src/js/download_shipment_order.js
+++ operations_warehouse/static/src/js/download_shipment_order.js
@@ -1,4 +1,4 @@
-var AbstractAction = require('web.AbstractAction');
+/*var AbstractAction = require('web.AbstractAction');
 var core = require('web.core');
 var rpc = require('web.rpc');
 var PrintAction = AbstractAction.extend(
@@ -32,5 +32,43 @@ var PrintAction = AbstractAction.extend(
 )
 	
 core.action_registry.add('print_action', PrintAction);
-return PrintAction;
+return PrintAction; */
+
+/**@odoo-module */
+const { Component, onMounted } = owl;
+import { registry } from "@web/core/registry";
+const actionRegistry = registry.category("actions");
+const rpc = require('web.rpc');
+export class PrintAction extends Component {
+    setup(){
+        super.setup(...arguments);
+        onMounted(()=>{
+            this.loadData();
+        })
+    }
+    loadData(){
+        let self = this;
+         rpc.query({
+            model: 'bossa.shipment.orders',
+            method: 'create_xlsx',
+        }).then(
+			function (result) {
+				console.log("================================")
+				console.log(result)
+				console.log("================================")
+
+				const link = document.createElement("a");
+				link.href = result;
+				link.download = "ShipmentOrder.xlsx"
+				link.click();
+			}
+		).catch(
+			function(error) {
+				console.error("Error during RPC call:", error);
+			}
+		);
+	};
+}
+
+actionRegistry.add('print_action', PrintAction);
 
