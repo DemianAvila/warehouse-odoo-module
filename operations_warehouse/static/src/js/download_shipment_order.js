0e7fcebf49d3bb0f374e851265fd720228fce52e
Demian Avila <demian.avila@bossa.com.mx>
2024-01-18 00:01:30 -0600

    fix: corrected args in method

--- operations_warehouse/static/src/js/download_shipment_order.js
+++ operations_warehouse/static/src/js/download_shipment_order.js
@@ -1,42 +1,6 @@
-/*var AbstractAction = require('web.AbstractAction');
-var core = require('web.core');
-var rpc = require('web.rpc');
-var PrintAction = AbstractAction.extend(
-	{
-		start: function() {
-			this._super.apply(this, arguments);
-			rpc.query(
-				{
-					model: 'bossa.shipment.orders',
-					method: 'create_xlsx',
-					args: [{}]
-				}
-			).then(
-				function (result) {
-					console.log("================================")
-					console.log(result)
-					console.log("================================")
-
-					const link = document.createElement("a");
-					link.href = result;
-					link.download = "ShipmentOrder.xlsx"
-					link.click();
-				}
-			).catch(
-				function(error) {
-					console.error("Error during RPC call:", error);
-				}
-			);;
-		}
-	}
-)
-	
-core.action_registry.add('print_action', PrintAction);
-return PrintAction; */
-
 /**@odoo-module */
-const { Component, onMounted } = owl;
 import { registry } from "@web/core/registry";
+const { Component, onMounted } = owl;
 const actionRegistry = registry.category("actions");
 const rpc = require('web.rpc');
 export class PrintAction extends Component {
