15a453dd53a80c1c3b19e45c1777fbd0834b3a2c
Demian Avila <demian.avila@bossa.com.mx>
2024-01-18 00:32:28 -0600

    test: debig method

--- operations_warehouse/models/shipment_order.py
+++ operations_warehouse/models/shipment_order.py
@@ -172,7 +172,7 @@ class ShipmentOrderInherit(models.Model):
         order = self.search_sale_orders(self.datetime_from, self.datetime_until)
         self.shipment_table = self.format_datatable(order)
         logging.info(json.dumps(order))
-        self.shipment_data = json.dumps(order)
+        self.shipment_data = str(json.dumps(order))
 
     @api.model
     def create_xlsx(self):
