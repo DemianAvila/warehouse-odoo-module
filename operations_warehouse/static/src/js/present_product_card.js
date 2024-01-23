6c4107ad4ffe494e4bd95e861d60783cc2647d93
Demian Avila <demian.avila@bossa.com.mx>
2024-01-23 17:04:20 -0600

    feat; change logic of card with widget

--- operations_warehouse/static/src/js/present_product_card.js
+++ operations_warehouse/static/src/js/present_product_card.js
@@ -1,7 +1,7 @@
 /**@odoo-module */
 const FormController = require('web.FormController');
-const { Component, useState, onMounted } = owl;
-const core = require('web.core');
+const { Component, useState, status } = owl;
+import { registry } from "@web/core/registry";
 
 export class InternalCodeListener extends FormController{
 	scannedInternalBarcode (value) {
@@ -18,9 +18,10 @@ export class InternalCodeListener extends FormController{
 
 
 export class ProductCard extends Component {
-	static template = "operations_warehouse.product_id";
+	static template = "operations_warehouse.ProductCard";
 	setup(){
 		console.log("access the state")
+		console.log(status)
         super.setup();
 		this.state = useState({
 			product_name: "a",
@@ -32,7 +33,8 @@ export class ProductCard extends Component {
 		});
     }
 }
-ProductCard.template = 'operations_warehouse.ProductCard';
+
+registry.category("fields").add("product_card",ProductCard)
 
 
 
