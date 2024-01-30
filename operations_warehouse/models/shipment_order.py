bd4a61a63ce73e3132ae040a1f9f2f13d273b9c9
Demian Avila <demian.avila@bossa.com.mx>
2024-01-29 23:44:09 -0600

    fix: document update

--- operations_warehouse/models/shipment_order.py
+++ operations_warehouse/models/shipment_order.py
@@ -116,10 +116,9 @@ class TmpGuides(models.TransientModel):
             # COMPARE THE NON ERASED IDS IF THE MODEL, ERASE THE ONES NOT ON THE LIST
             logging.info("==========================")
             logging.info("get guides of this model")
-            logging.info(vals["guides"][2]["ext_id"])
             logging.info(self.guides)
             logging.info("==========================")
-            guides_ids = [guide for guide in vals["guides"][2]["ext_id"]]
+            guides_ids = [guide[2]["ext_id"] for guide in vals["guides"]]
             logging.info("==========================")
             logging.info("get guides of this model")
             logging.info(guides_ids)
